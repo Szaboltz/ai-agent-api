30c0e3109370ca9032456390e20763c605c92101
Szabo Henrique <szaboprofissional@gmail.com>
2025-03-13 22:21:34 -0300

    done

---
+++ src/controllers/initial.controller.js
@@ -0,0 +1,5 @@
+export default function initialController(req, res) {
+  return res.json({
+    message: 'Olá! Sou um especialista em Análise e Desenvolvimento de Sistemas, pronto para responder suas perguntas sobre o curso, o campus de Caraguatatuba e o Instituto Federal de São Paulo. 😉'
+  })
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import aiController from './controllers/ai.controller.js'
 import cors from 'cors'
+import initialController from './controllers/initial.controller.js'
 
 const app = express()
 const port = 3000
@@ -8,11 +9,9 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Olá Mundo!')
-})
-
 app.post('/ai', aiController)
+
+app.get('/init', initialController)
 
 app.listen(port, () => {
   console.log(`App de exemplo esta rodando na porta ${port}`)
